1bb4a700552da43857200635e5c64757cbb33e20
Gene Su <13686017469@163.com>
2018-07-05 18:05:26 +0800

    html print with button.

---
+++ test3.rb
@@ -0,0 +1,5 @@
+require 'ooor'
+Ooor.new(url: 'https://pstest-maxmore6.odoo.com/jsonrpc', database: 'pstest-maxmore6', username: 'hr1@maxmore.asia', password: '1234567')
+
+# ResUsers.find(1)
+ProductProduct.find(:last)
